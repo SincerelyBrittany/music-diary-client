bbdf7e5ae5459847ebf0131597ee77116f29bab2
SincerelyBrittany <sincerelybr@gmail.com>
2020-11-29 09:33:42 -0500

    adds create store, persist store, and rootreducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom'
+import { createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/rootReducer'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2 
+}
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+
+let store = createStore(pReducer, composeWithDevTools(applyMiddleware(thunk)))
+let persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <PersistGate persistor={persistor} >
+          <App />
+        </PersistGate>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
