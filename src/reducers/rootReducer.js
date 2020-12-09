6a305ddb52ced7db6f7f9d598d2d450bd6159830
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-09 09:46:46 -0500

    comments out imports

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import commentsReducer from "./commentsReducer";
-import searchReducer from "./searchReducer";
+// import commentsReducer from "./commentsReducer";
+// import searchReducer from "./searchReducer";
 import userReducer from './userReducer'
-import loginReducer from './loginReducer'
+
 
  
 const appReducer = combineReducers({
