d31c6619ef944f41b693a66d3f67e7b13e3b32cf
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-11 13:18:57 -0500

    adds function to submit fetch request to add song in song card component

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -1,11 +1,27 @@
 import React, { useRef } from 'react';
-
+import { connect } from 'react-redux';
+import { useHistory } from "react-router-dom";
+import { selectSong } from '../actions/actionCreators'
 
 const SongCard = (props) => {
+  let history = useHistory();
   const audioRef = useRef()
   const {name, artist, image, preview, spotify_id, lyrics} = props
+  const handleClick = (event) => {
+    //Make fetch request to save props/song
+    //save props in state
+    //go to next page and save user id and song_id
+    // console.log(this.props, "in song card")
+   
+    console.log(props, "in song card")
+    // console.log(this, "this is this")
+    props.selectSong(props)
+    history.push("/selectedsong");
+    // this.props.history.push('/selectedsong')
+  }
+
   return (
-    <div className="card" onClick={props.onClick}>
+    <div className="card" >
       <img src={image} alt={name}/><br/>
       <p>{name}</p>
       <p> {artist} </p>
@@ -13,8 +29,15 @@ const SongCard = (props) => {
       <audio controls ref={audioRef}>
       <source src={preview} type='audio/mpeg' />
       </audio>
+      <button onClick={handleClick} data-tag={spotify_id}> Pick this song for the day </button>
     </div>
   )
 }
 
-export default SongCard
+
+function mapDispatchToProps(dispatch){
+  return { selectSong: (song) => dispatch(selectSong(song)) }
+}
+
+export default connect(null, mapDispatchToProps)(SongCard)
+
