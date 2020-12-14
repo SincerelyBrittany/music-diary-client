01eec9d49c06500a49425559dd9ae9f09fbb9ad1
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-13 21:20:57 -0500

    removes comments

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -4,32 +4,29 @@ import { useHistory } from "react-router-dom";
 import { selectSong } from '../actions/actionCreators'
 
 const SongCard = (props) => {
-  let history = useHistory();
-  const audioRef = useRef()
-  const {name, artist, image, preview, spotify_id, lyrics} = props
-  const handleClick = (event) => {
-    //Make fetch request to save props/song
-    //save props in state
-    //go to next page and save user id and song_id
-    // console.log(this.props, "in song card")
-   
-    console.log(props, "in song card")
-    // console.log(this, "this is this")
-    props.selectSong(props)
-    history.push("/selectedsong");
-    // this.props.history.push('/selectedsong')
-  }
+    let history = useHistory();
+    const audioRef = useRef()
+    const {name, artist, image, preview, spotify_id, lyrics} = props
+
+      const handleClick = (event) => {
+        props.selectSong(props)
+        history.push("/selectedsong");
+      }
 
   return (
     <div className="card" >
       <img src={image} alt={name}/><br/>
-      <p>{name}</p>
-      <p> {artist} </p>
-      <a href={lyrics}>Link to {name} Lyrics</a>
-      <audio controls ref={audioRef}>
-      <source src={preview} type='audio/mpeg' />
+        <h1>{name}</h1>
+        <h3> By: {artist} </h3>
+        <a href={lyrics} className='new-line'>Link to {name} Lyrics</a>
+      <br></br>
+      <br />
+      <audio controls ref={audioRef} className='new-line'>
+      <source src={preview} className='new-line' type='audio/mpeg' />
       </audio>
-      <button onClick={handleClick} data-tag={spotify_id}> Pick this song for the day </button>
+      <br />
+      <br></br>
+      <button className="card-btn" onClick={handleClick} data-tag={spotify_id}> Pick this song for the day </button>
     </div>
   )
 }
