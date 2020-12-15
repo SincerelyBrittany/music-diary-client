ff39e646110fd1aa81635c18c1ae3c8cf0cb73cd
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-15 10:56:52 -0500

    changes to class component to make fetch request upon component did mount

--- src/containers/PastEntryPage.js
+++ src/containers/PastEntryPage.js
@@ -1,39 +1,87 @@
 import React, {Component, useState, useEffect} from 'react'
 import { connect } from 'react-redux'
+import PastSongCard from '../components/PastSongCard'
+// const OldEntry = props => {
+//     const [songs, setSongs] = useState([])
+//     useEffect(() => {
+//       const api_url = "http://localhost:3000/api/v1/get_user_entries";
+//           fetch(api_url, {
+//             method: 'POST', 
+//             headers: {
+//               'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({user: props.user}),
+//           })
+//           .then(response => response.json())
+//         //   .then(results => {
+//         //     console.log(results)
+//         // })
+//         .then(response => {
+//           setSongs(response);
+//       });
 
-const OldEntry = props => {
-    const [songs, setSongs] = useState([])
-    useEffect(() => {
-      const api_url = "http://localhost:3000/api/v1/get_user_entries";
-          fetch(api_url, {
-            method: 'POST', 
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({user: props.user}),
-          })
-          .then(response => response.json())
-        //   .then(results => {
-        //     console.log(results)
-        // })
-        .then(response => {
-          setSongs(response);
-      });
+//       }, [songs])
 
-      })
+//       console.log(songs, "this is songs")
 
-      console.log(songs, "this is songs")
+//       const content = songs.length !== 0 ? songs.map(song=> (<li key={song.update_date}>{song.song.name}</li> )) : 'no song found'
+//     return(
 
-      const content = songs.length !== 0 ? songs.map(song=> (<li key={song.update_date}>{song.song.name}</li> )) : 'no song found'
-    return(
+//       <div>
+//         <ul>
+//          {content}
+//         </ul>
+//       </div>
+//     )
+// }'
 
-      <div>
-        <ul>
-         {content}
-        </ul>
-      </div>
-    )
+class OldEntry extends Component {
+  // const [songs, setSongs] = useState([])
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+      //   data: null,
+      data: [],
+      };
+    }
+ 
+  componentDidMount() {
+    const api_url = "http://localhost:3000/api/v1/get_user_entries";
+        fetch(api_url, {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({user: this.props.user}),
+        })
+        .then(response => response.json())
+      //   .then(results => {
+      //     console.log(results)
+      // })
+      // .then(response => {
+      .then(data => this.setState({ data }));
+        // setSongs(response);
+    // });
+
+    }
+
+render(){
+    console.log(this.state.data, " this is the data given back")
+    const content = this.state.data.length !== 0 ? this.state.data.map(song=> (<PastSongCard key={song.spotify_id} {...song} /> )) : 'no song found'
+  return(
+
+    <div>
+      <ul>
+       {content}
+      </ul>
+    </div>
+  )
 }
+}
+
+
+
 
 const msp = (state) => ({
     user: state.user.id
