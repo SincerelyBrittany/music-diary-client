51f9de36fe8f60e106ee72b884b34897496f48f8
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-15 10:11:36 -0500

    adds past song card to render all card for user

---
+++ src/components/PastSongCard.js
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import { useHistory } from "react-router-dom";
+import { selectSong } from '../actions/actionCreators'
+
+const PastSongCard = (props) => {
+    let history = useHistory();
+    const audioRef = useRef()
+    const {name, artist, image, preview, spotify_id, lyrics} = props.song
+    const {description } = props.description
+    const {date } = props.update_date
+
+      const handleClick = (event) => {
+        props.selectSong(props)
+        history.push("/selectedsong");
+      }
+
+  return (
+    <div className="card" >
+      <img src={image} alt={name}/><br/>
+        <h1>{name}</h1>
+        <h3> By: {artist} </h3>
+        <a href={lyrics} className='new-line'>Link to {name} Lyrics</a>
+      <br></br>
+      <br />
+      <audio controls ref={audioRef} className='new-line'>
+      <source src={preview} className='new-line' type='audio/mpeg' />
+      </audio>
+      <br />
+      <br></br>
+      <p> {description} </p>
+      {/* <button className="card-btn" onClick={handleClick} data-tag={spotify_id}> Pick this song for the day </button> */}
+    </div>
+  )
+}
+
+
+function mapDispatchToProps(dispatch){
+  return { selectSong: (song) => dispatch(selectSong(song)) }
+}
+
+export default connect(null, null)(PastSongCard)
+
