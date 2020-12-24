05ca897ca7162927e8f7b4f6c8c656ce517b62f3
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-24 17:12:32 -0500

    creates click event to show user, date, and description in console

--- src/containers/AllEntries.js
+++ src/containers/AllEntries.js
@@ -9,11 +9,22 @@ class AllEntries extends Component{
           this.props.getAllEntries()
       }
 
-      handleClick = (e) => {
+      handleCommentClick = (e) => {
           let id = e.target.dataset.id
           this.props.viewComments(id)
           this.props.history.push("/comments")
       }
+
+      handleEntryClick = (e) => {
+        let currentID = e.target.dataset.id
+        console.log(this.props, "you are in entries looking at the id")
+        const result = this.props.entries.results.filter(entry => entry.id == currentID )
+        console.log(result[0])
+        debugger
+        let description = result[0].description
+        let user = result[0].user.username
+        let date = result[0].update_date
+    }
 
       renderEntries(){
         return this.props.entries.results.map((card) => {
@@ -23,13 +34,13 @@ class AllEntries extends Component{
                       <h3>Song: {card.song.name}</h3>
                       <h3>Artist: {card.song.artist}</h3>
                       <img src={card.song.image} alt="Image" />
-                      <button data-id={card.id} onClick={this.handleClick}> View All Comments/ Comment on Diary Entry  </button>
+                      <button data-id={card.id} onClick={this.handleCommentClick}> View All Comments/ Comment on Diary Entry  </button>
+                      <button data-id={card.id} onClick={this.handleEntryClick}> View Entry  </button>
                 </div>
         })
     } 
 
       render(){
-          console.log(this.props.entries.results, "this is props in entry")
         //   if (this.props.loading) {
         //     return (
         //         <LoadingContainer/>
