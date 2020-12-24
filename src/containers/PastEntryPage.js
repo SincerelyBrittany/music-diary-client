d79137e79788618e5cdd96678befa576e26de03b
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-24 17:11:50 -0500

    removes comments

--- src/containers/PastEntryPage.js
+++ src/containers/PastEntryPage.js
@@ -1,4 +1,4 @@
-import React, {Component, useState, useEffect} from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import PastSongCard from '../components/PastSongCard'
 import Nav from '../components/Navbar'
