eed854171af28e0d309d313509d7f04bdb6d801b
SincerelyBrittany <sincerelybr@gmail.com>
2021-01-01 20:48:40 -0500

    shows errors when not successfully loaded by returning props.errors

--- src/containers/SelectedSongPage.js
+++ src/containers/SelectedSongPage.js
@@ -1,4 +1,5 @@
 import React, { useRef, Component  } from 'react';
+import MainPage from './MainPage'
 import { connect } from 'react-redux'
 import { useHistory } from "react-router-dom";
 import { songOfTheDay, handleSongFormChange } from '../actions/actionCreators'
@@ -43,10 +44,13 @@ const handleSubmit = (event) => {
     </div>
   )}
   else {
+    return(
     <div>
       <p> This is props</p>
       <p> {props.errors} </p>
+      <MainPage />
     </div>
+    )
   }
 }
 
